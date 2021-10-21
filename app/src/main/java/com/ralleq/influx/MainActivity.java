832157dd67926594084b44bf3ab95ea16839c28b
Rasmus Krogh Udengaard <53310290+ralleq@users.noreply.github.com>
2021-10-21 18:18:13 +0200

    Added LogCat entry for testing GitHub

--- app/src/main/java/com/ralleq/influx/MainActivity.java
+++ app/src/main/java/com/ralleq/influx/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
@@ -28,6 +29,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("HI", "GitHub test");
         super.onCreate(savedInstanceState);
         audioRouter = new AudioRouter(this, new AudioRunnable() {
             @Override
